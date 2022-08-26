4eb84d853843bc178c2428fd44774178c293f0f9
Pasunhey <kkk5993@naver.com>
2022-08-26 10:06:11 +0900

    22.08.26
    10:26

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-let d = 1;
-function gallery(num){
-    if(num){
-        if(num===2) d++;
-    }
-    else{
-        if(num ===1) d--;
-    }
+// const hen = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => resolve('😊'), 1000);
+//     });
+// }
+
+// const egg = (hen) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => resolve(`${hen} => 😘`), 1000);
+//     });
+// }
+
+// const cook = (egg) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => resolve(`${egg} => 😉`), 1000);
+//     })
+// }
+
+// console.log(
+//     hen()
+//         .then(hen => egg(hen))
+//         .then(egg => cook(egg))
+//         .then(console.log)
+// );
+
+const obj = {
+    a : 100,
+    b : 200,
+    c : 'javascript',
 }
-gallery(2);
-console.log(num);
+
+const {a : name1, b :name2, c : name3} = obj;
+console.log(name1)
+console.log(name2)
+console.log(name3)
