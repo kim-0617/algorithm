6bc566a91b5d7394a628e84250fcab46cde8b44a
Pasunhey <kkk5993@naver.com>
2022-10-05 10:32:38 +0900

    22.10.05
    10:32

--- index.js
+++ index.js
@@ -33,3 +33,38 @@
 // console.log(name1)
 // console.log(name2)
 // console.log(name3)
+
+
+function q1() {
+    let maxVal = Math.max(100, Math.min(-100, Math.random() * 1000));
+    return maxVal;
+}
+
+console.log(q1());
+
+function q2() {
+    const array = [10, 31, 1, 2, 3 ,4, 32];
+    return array.sort();
+}
+
+console.log(q2());
+
+function q3() {
+    const array = [10, 31, 1, 2, 3 ,4, 32];
+    return array.filter(x => Number(String(x).length >= 2));
+}
+
+console.log(q3());
+
+function q4() {
+    const array = [10,20,30,40,50];
+    return array.reduce((prev, curr) => prev += curr, 0) / array.length;
+}
+
+console.log(q4());
+
+function q5() {
+    return ""
+}
+
+console.log(q4());
