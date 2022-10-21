b61981f8705fe1bd42a3492c0482eefa0caa7a27
Pasunhey <kkk5993@naver.com>
2022-10-21 10:17:38 +0900

    22.10.21
    10:17

--- index.js
+++ index.js
@@ -144,17 +144,40 @@
 
 // console.log(q1(1996));
 
-function solution(s) {
-    let answer;
-    let stack = [];
-    for (let x of s) {
-        if (x === ')') {
-            while (stack.pop() !== '(');
+// function solution(s) {
+//     let answer;
+//     let stack = [];
+//     for (let x of s) {
+//         if (x === ')') {
+//             while (stack.pop() !== '(');
+//         }
+//         else stack.push(x);
+//     }
+//     answer = stack.join('');
+//     return answer;
+// }
+// let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+// console.log(solution(str));
+
+function solution(arr1, arr2) {
+    let answer = [];
+    arr1.sort();
+    arr2.sort();
+
+    let p1 = p2 = 0;
+    while(p1 < arr1.length && p2 < arr2.length) {
+        console.log("1",p1, p2);
+        if(arr1[p1] === arr2[p2]) {
+            answer.push(arr1[p1++]);
+            p2++;
+            console.log("2",p1, p2);
         }
-        else stack.push(x);
+        else if(arr1[p1] < arr2[p2]) p1++;
+        else p2++;
     }
-    answer = stack.join('');
     return answer;
 }
-let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
-console.log(solution(str));
+
+let a = [1,3,9,5,2];
+let b = [3,2,5,7,8];
+console.log(solution(a,b));
