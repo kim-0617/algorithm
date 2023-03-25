50c089ac2e98978e5f7c06cd45365c0ee53efa1c
kim-0617 <kkk5993@naver.com>
2023-03-25 16:40:35 +0900

    23.03.25

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-function solution(fees, records) {
-  let answer = [];
-  const limit = 1439;
+const fs = require("fs");
 
-  const candidate = [];
-  for (let i = 0; i < records.length; i++) {
-    let target = records[i].split(" ")[1];
-    if (!candidate.includes(target)) {
-      candidate.push(target);
-    }
+const filelistArray = [];
+fs.readdir("./01", (error, filelist) => {
+  for (let i = 0; i < filelist.length; i++) {
+    filelistArray.push(filelist[i]);
   }
-  candidate.sort();
 
-  let obj = {};
-  candidate.forEach((x, i) => {
-    obj[x] = records.filter((y) => y.split(" ")[1] === x);
-    if (obj[x].length % 2 === 1) {
-      obj[x].push(`23:59 ${x} OUT`);
-    }
-  });
-  console.log(Object.keys(obj).sort());
+  console.log(filelistArray.length);
+});
 
-  return answer;
-}
-
-const fees = [180, 5000, 10, 600];
-const records = [
-  "05:34 5961 IN",
-  "06:00 0000 IN",
-  "06:34 0000 OUT",
-  "07:59 5961 OUT",
-  "07:59 0148 IN",
-  "18:59 0000 IN",
-  "19:09 0148 OUT",
-  "22:59 5961 IN",
-  "23:00 5961 OUT",
-];
-
-solution(fees, records);
+console.log("first");
