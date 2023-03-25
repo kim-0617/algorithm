ed578cada1c4b3fd7c376d255f00f1dcf9e59ebc
kim-0617 <kkk5993@naver.com>
2023-03-25 17:00:01 +0900

    readme 수정

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 
 const filelistArray = [];
-fs.readdir("./01", (error, filelist) => {
+fs.readdir("./02", (error, filelist) => {
   for (let i = 0; i < filelist.length; i++) {
     filelistArray.push(filelist[i]);
+    console.log("- ", filelist[i]);
   }
-
-  console.log(filelistArray.length);
 });
 
 console.log("first");
